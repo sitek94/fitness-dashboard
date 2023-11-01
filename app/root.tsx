990b37f9522143bf67fc8a3a554fc42d01a5246f
Maciek Sitkowski <msitkowski94@gmail.com>
2023-11-01 10:45:34 +0100

    setup eslint and prettier

--- app/root.tsx
+++ app/root.tsx
@@ -1,42 +1,42 @@
-import {cssBundleHref} from '@remix-run/css-bundle'
-import type {LinksFunction} from '@remix-run/node'
+import { cssBundleHref } from '@remix-run/css-bundle'
+import { type LinksFunction } from '@remix-run/node'
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
 } from '@remix-run/react'
 import faviconUrl from './assets/favicon.svg'
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref
-    ? [{rel: 'stylesheet', href: cssBundleHref}]
-    : [
-        {
-          rel: 'icon',
-          type: 'image/svg+xml',
-          href: faviconUrl,
-        },
-      ]),
+	...(cssBundleHref
+		? [{ rel: 'stylesheet', href: cssBundleHref }]
+		: [
+				{
+					rel: 'icon',
+					type: 'image/svg+xml',
+					href: faviconUrl,
+				},
+		  ]),
 ]
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	)
 }
